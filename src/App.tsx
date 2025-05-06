b92be4e218a9a7db599ddd77dde2b5fbdc08c52c
Dávid Kasznár <kasznardavid8@gmail.com>
2025-05-06 12:16:02 +0200

    connect to api, typescirpt setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useQuery, gql} from "@apollo/client";
+// import {gql} from "./__generated__/gql";
+
+
+const GET_USER_REPOS = gql`
+    query GetUserRepos($login: String!) {
+        user(login: $login) {
+            repositories(first: 10, orderBy: { field: UPDATED_AT, direction: DESC }) {
+                nodes {
+                    name
+                    description
+                    url
+                    updatedAt
+                    stargazerCount
+                }
+            }
+        }
+    }
+`;
+
+
+function DisplayLocations() {
+    const { data, loading, error } = useQuery(GET_USER_REPOS, {
+        variables: { login: 'kasznar' },
+    });
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error : {error.message}</p>;
+
+    return <pre>{JSON.stringify(data, null, 4)}</pre>
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <DisplayLocations />
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {ApolloClient, InMemoryCache, ApolloProvider, HttpLink} from '@apollo/client';
+
+
+const client = new ApolloClient({
+    link: new HttpLink({
+        uri: 'https://api.github.com/graphql',
+        headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
+        },
+    }),    cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <ApolloProvider client={client}>
+          <App />
+      </ApolloProvider>,
   </StrictMode>,
 )
